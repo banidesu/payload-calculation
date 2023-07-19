3d15cf9a4903b51a3169ff91d8fedd43f2e8a06a
Robani <muhammadrobbani77@gmail.com>
2023-07-19 18:42:06 +0700

    route login & middleware prepare

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('actionLogin', [LoginController::class, 'actionLogin'])->name('actionLogin');
+
+Route::group(['middleware' => ['auth']], function () {
+    
+    Route::get('home', [HomeController::class, 'index'])->name('home');
+    Route::get('actionLogout', [LoginController::class, 'actionLogout'])->name('actionLogout');
 });
