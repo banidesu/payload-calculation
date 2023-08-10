3fc90df314c14ab529c04389197ae531165dd798
Robani <muhammadrobbani77@gmail.com>
2023-08-11 02:35:03 +0700

    modal tambah added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Halaman {{ $pageTitle }}</title>
     <link rel="shortcut icon" href="{{ asset('img/logo.jpg') }}" type="image/x-icon">
+    {{-- link bootstrap css --}}
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    {{-- link fontawesome css --}}
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome/css/all.min.css') }}">
 </head>
 
 <body>
 
     <section class="py-5 bg-light shadow" id="content">
         <div class="container-fluid">
-            <button>Tambah</button>
+            <button class="btn btn-primary mb-3" type="button" data-bs-toggle="modal" data-bs-target="#modalTambah"><i
+                    class="fa-solid fa-circle-plus fa-fw fa-fade"></i> Tambah</button>
             <div class="table-responsive rounded">
                 <table class="table table-primary shadow-sm">
                     <thead>
@@ -40,7 +44,8 @@
                         </tr>
                         <tr>
                             <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
+                            <td>Larry the Bird</td>
+                            <td>Bani</td>
                             <td>@twitter</td>
                         </tr>
                     </tbody>
@@ -49,7 +54,29 @@
         </div>
     </section>
 
+    {{-- Modal Tambah --}}
+    <div class="modal fade" id="modalTambah" tabindex="-1" aria-labelledby="modalTambahLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="modalTambahLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- link bootstrap js --}}
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    {{-- link fontawesome js --}}
+    <script src="{{ asset('vendor/fontawesome/js/all.min.js') }}"></script>
+
 </body>
 
 </html>
